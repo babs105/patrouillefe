836829c976dfb60c1fea54f1cb078837328dab01
babs105 <mamembaye105@live.fr>
2020-03-25 11:48:36 +0000

    fix bug6

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
  //baseURL: 'http://localhost:8080'
- baseURL: 'https://patrouille.herokuapp.com/'
+ baseURL: 'https://patrouille.herokuapp.com'
 });
