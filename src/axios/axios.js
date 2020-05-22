ee0532a1aff956b96d8af8db9e4325211f1b9213
babs105 <mamembaye105@live.fr>
2020-05-22 09:42:23 +0000

    add origin panne

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080'
-    // baseURL: 'https://patrouille.herokuapp.com'
+   // baseURL: 'http://localhost:8080'
+     baseURL: 'https://patrouille.herokuapp.com'
     
 });
